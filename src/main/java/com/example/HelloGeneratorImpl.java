4179ec1c0f006410f54646afd18209ffb8c3d028
Joëll Portier <JoellP@infosupport.com>
2022-12-23 09:59:58 +0100

    Print traceId and spanId

--- src/main/java/com/example/HelloGeneratorImpl.java
+++ src/main/java/com/example/HelloGeneratorImpl.java
@@ -1,14 +1,24 @@
 package com.example;
 
 import io.micronaut.tracing.annotation.NewSpan;
+import io.opentracing.Tracer;
 import jakarta.inject.Singleton;
 
 @Singleton
 public class HelloGeneratorImpl implements HelloGenerator{
 
+    private Tracer tracer;
+
+    public HelloGeneratorImpl(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
     @NewSpan
     @Override
     public String hello() {
+        var traceId = tracer.activeSpan().context().toTraceId();
+        var spanId = tracer.activeSpan().context().toSpanId();
+        System.out.println(String.format("h trace: %s, span: %s", traceId, spanId));
         return "Hello";
     }
 }
--- src/main/java/com/example/WorldGeneratorImpl.java
+++ src/main/java/com/example/WorldGeneratorImpl.java
@@ -1,12 +1,22 @@
 package com.example;
 
+import io.opentracing.Tracer;
 import jakarta.inject.Singleton;
 
 @Singleton
 public class WorldGeneratorImpl implements WorldGenerator {
 
+    private Tracer tracer;
+
+    public WorldGeneratorImpl(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
     @Override
     public String getName() {
+        var traceId = tracer.activeSpan().context().toTraceId();
+        var spanId = tracer.activeSpan().context().toSpanId();
+        System.out.println(String.format("w trace: %s, span: %s", traceId, spanId));
         return "World";
     }
 }
